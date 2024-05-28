1be862b95730b5577f89b59c891b44070243e2ae
Kaniltazo <angeldanielrm@icloud.com>
2024-05-27 19:00:12 -0500

    ajustes

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// querySelector 
+
+const heading = document.querySelector('.header__texto h2'); // 0 o 1 elemento
+
+heading.textContent = 'Nuevo Heading';
+
+console.log(heading);
+
+
+//querySelectorAll
+
+const enlaces = document.querySelectorAll('.navegacion a'); 
+console.log(enlaces);
+
+// enlaces[0].textContent = 'Nuevo texto para enlace'
+
+// enlaces[0].classList.add('nueva-clase');
+
+// enlaces[0].href = 'http://google.com/search'
+
+
+const nuevoEnlace = document.createElement()
+
+
+
+
+
